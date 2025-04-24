6f6a5030d3f61a101f87e2dae5efa1afc355953f
Satyam018 <83343048+Satyam018@users.noreply.github.com>
2025-04-24 19:25:18 +1000

    Update DeepNeuron.py
    adding back propagation

--- DeepNeuron.py
+++ DeepNeuron.py
@@ -2,10 +2,7 @@ import numpy as np
 from Activation import Relu_activation,Softmax_activation
 from Loss import Cross_entropy_loss
 
-inputs=[[1.2,3.4,5.6],
-        [7.8,9.0,1.2]]
 
-y_true=[0,1]
 
 class Dense_layer:
     def __init__(self,input_size,neuron_number):
@@ -20,37 +17,39 @@ class Dense_layer:
         weighted_input=np.dot(inputs,self.weights)
         self.output=weighted_input+self.biases
 
-        
 
 
-        
+    def back_propagation(self,output_gradient,inputs):
+        self.dweights=np.dot(output_gradient,inputs.T)
+        self.dinputs=np.dot(self.weights.T,output_gradient)
+        self.dbiases=np.sum(output_gradient,axis=0,keepdims=True)
 
-        
-
-   
-
-
-
+    
 
 
-layer1=Dense_layer(3,3)
-layer1.forward(inputs)
+if __name__=="__main__":
 
 
-relu=Relu_activation()
-relu.activate_forward(layer1.output)
+    inputs=[[1.2,3.4,5.6],
+            [7.8,9.0,1.2]]
 
-layer2=Dense_layer(3,2)
-layer2.forward(relu.output)
-softmax=Softmax_activation()
-softmax.activate_forward(layer2.output)
+    y_true=[0,1]
 
-loss=Cross_entropy_loss()
-print('loss',loss.calculate_loss(y_true,softmax.output))
+    layer1=Dense_layer(3,4)
+    layer1.forward(inputs)
 
 
+    relu=Relu_activation()
+    relu.activate_forward(layer1.output)
 
-# print('softmax',softmax.output)
+    layer2=Dense_layer(4,2)
+    layer2.forward(relu.output)
+    softmax=Softmax_activation()
+    softmax.activate_forward(layer2.output)
+
+    loss=Cross_entropy_loss()
+    print('loss',loss.calculate_loss(y_true,softmax.output))
+
 
 
 
