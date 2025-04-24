4a4adf53ddcd1fa891fb2e8b4732f9f577fbcbd7
Satyam018 <83343048+Satyam018@users.noreply.github.com>
2025-04-24 19:27:32 +1000

    Update Activation.py

--- Activation.py
+++ Activation.py
@@ -9,6 +9,12 @@ class Relu_activation(Activation):
 
     def activate_forward(self,inputs):
         self.output=np.maximum(0,inputs)
+
+    def back_propagation(self,dinputs):
+        self.doutputs[dinputs<=0]=0
+        return self.doutputs
+        
+
         
 
 
@@ -20,3 +26,7 @@ class Softmax_activation(Activation):
         exp_inputs=np.exp(scale_down)
         self.output=exp_inputs/np.sum(exp_inputs,axis=1,keepdims=True)
     
+    def back_propagation(self, dinputs): 
+    # Assumes dinputs is the true labels (one-hot or adjusted gradient)
+        self.doutputs = self.output - dinputs
+        return self.doutputs
